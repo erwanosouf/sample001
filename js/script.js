a5018ed1b01d71445c7aeb6005f2db1b3927630d
Erwan Osouf <erwan.osouf@gmail.com>
2017-11-03 11:21:06 +0100

    Add ressources

--- js/script.js
+++ js/script.js
@@ -12,6 +12,33 @@ var text =
 + "Then - in the name of democracy - let us use that power - let us all unite. Let us fight for a new world - a decent world that will give men a chance to work - that will give youth a future and old age a security. By the promise of these things, brutes have risen to power. But they lie! They do not fulfil that promise. They never will!"
 + "Dictators free themselves but they enslave the people! Now let us fight to fulfil that promise! Let us fight to free the world - to do away with national barriers - to do away with greed, with hate and intolerance. Let us fight for a world of reason, a world where science and progress will lead to all men’s happiness. Soldiers! in the name of democracy, let us all unite!"
 
+var text2 =
+"On the question of racial discrimination, the Addis Ababa Conference taught, to those who will learn, this further lesson:"
++ "that until the philosophy which holds one race superior and another inferior is finally and permanently discredited and abandoned;"
++ "that until there are no longer first class and second class citizens of any nation;"
++ "that until the color of a man's skin is of no more significance than the color of his eyes;"
++ "that until the basic human rights are equally guaranteed to all without regard to race;"
++ "that until that day, the dream of lasting peace and world citizenship and the rule of international morality will remain but a fleeting illusion, to be pursued but never attained."
++ "And until the ignoble and unhappy regimes that hold our brothers in Angola, in Mozambique and in South Africa in subhuman bondage have been toppled and destroyed;"
++ "until bigotry and prejudice and malicious and inhuman self-interest have been replaced by understanding and tolerance and good-will;"
++ "until all Africans stand and speak as free beings, equal in the eyes of all men, as they are in the eyes of Heaven;"
++ "until that day, the African continent will not know peace. We Africans will fight, if necessary, and we know that we shall win, as we are confident in the victory of good over evil.";
+
+var text3 =
+"We had two bags of grass, seventy-five pellets of mescaline, five sheets of high-powered blotter acid, a saltshaker half-full of cocaine, and a whole galaxy of multi-colored uppers, downers, laughers, screamers..."
++ "Also, a quart of tequila, a quart of rum, a case of beer, a pint of raw ether, and two dozen amyls."
++ "Not that we needed all that for the trip, but once you get into locked a serious drug collection, the tendency is to push it as far as you can."
++ "The only thing that really worried me was the ether. There is nothing in the world more helpless and irresponsible and depraved than a man in the depths of an ether binge, and I knew we'd get into that rotten stuff pretty soon."
+
+
+var text4 =
+"Dog carcass in alley this morning. Tire tread on burst stomach. The city is afraid of me.I have seen it's true face."
++ "The streets are extended gutters and the gutters are full of blood and when the drains finally scab over all the vermin will drown."
++ "The accumulated filth of all their sex and murder will foam up about their waists and and all the whores and politicians will look up and shout \"Save us!\"... and I'll whisper... \"No.\""
++ "They had a choice, all of them. They could have followed in the footsteps of good men like my father, or president Truman."
++ "Decent men who believed in a day's work for a day's pay. Instead they followed the droppings of lechers and communists and didn't realize that the trail led over a precipice until it was too late."
++ "Don't tell me they didn't have a choice. Now the whole world stands on the brink, staring down into bloody Hell, all those liberals and intellectuals and smooth talkers...and all of a sudden nobody can think of anything to say."
+
 var img = new Image();
 
 img.onload = function() {
@@ -28,29 +55,30 @@ img.onload = function() {
 
 	getData(img, asMatrix);
 
-	renderAsHtml(pixels, text, img.width, img.height, renderCell1);
-	// renderAsSvg([
-	// 	1, 2, 3,
-	// 	4, 5, 6,
-	// 	7, 8 ,9], 'abc', 3, 3);
+	renderAsHtml(pixels, text4, img.width, img.height, renderCell1);
+	//renderAsSvg(pixels, 'abcdefghi', 3, 3);
 }
 
 function renderAsSvg(pixels, text, width, height) {
-	var scale = 12, w =  width * scale, h = height * scale;
+	var scale = 100, w =  width * scale, h = height * scale;
 
 	var svg = d3.select("body").append("svg")
     .attr("width", w)
     .attr("height", h);
-		console.log(pixels);
-	var g = svg.selectAll('text')
+  text = svg.append("text");
+		// console.log(pixels);
+	var g = text.selectAll('tspan')
 			.data(pixels)
 			.enter()
-				.append('text')
+        //.append('rect')
+        .attr('x', function(d) { return d.x * scale})
+				.attr('y', function(d, i) { return (d.y + 1) *scale /* for text, y is the BOTTOM of the text */})
+				.attr('width', scale)
+				.attr('height', scale)
+				//.append('text')
 				//.attr('style', 'fill:white;')
-				.attr('x', function(d) { return (d % width) * scale; })
-				.attr('y', function(d, i) { return parseInt(d / width) * scale; })
-				//.attr('width', w)
-				//.attr('height', scale)
+        .attr('textLength', scale)
+        .attr('font-size', scale)
 				.text('a')
 				.attr('fill','black');
 
@@ -137,4 +165,4 @@ function asLevel(color) {
 
 
 //Loads the image
-img.src = 'img/dictator.jpg';
+img.src = 'img/rorschach.jpg';
